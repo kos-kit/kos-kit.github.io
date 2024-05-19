123ef7932c9e227071ec0ae1e339c4526e4f9a8d
Minor Gordon <minorg@server.fake>
2024-05-19 12:07:54 -0400

    remove existing sidebars

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,7 +35,6 @@ const config: Config = {
       {
         docs: {
           routeBasePath: "/",
-          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
